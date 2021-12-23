bac914c5f7b685da17a6dff6f8e9734c61940885
AFusan <afukunsan@outlook.com>
2021-12-23 13:52:10 +0800

    first commit

---
+++ install-luci-app-clash.sh
@@ -0,0 +1,2 @@
+opkg install --force-overwrite bash coreutils coreutils-nohup coreutils-base64 ipset iptables luci luci-base luci-compat wget libustream-openssl curl jsonfilter ca-certificates iptables-mod-tproxy kmod-tun
+opkg install luci-app-clash_v1.7.5.7_all.ipk
